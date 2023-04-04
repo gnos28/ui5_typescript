79d08bc40a76d686a132c36b3a5060fbf6a61981
gnos28 <gnos28@gmail.com>
2023-04-04 10:45:25 +0200

    exo4 done

--- src/controller/Main.controller.ts
+++ src/controller/Main.controller.ts
@@ -1,13 +1,17 @@
-import MessageBox from "sap/m/MessageBox";
+import { IconColor } from "sap/ui/core/library";
 import BaseController from "./BaseController";
 
 /**
  * @namespace com.myorg.tsexo1.controller
  */
 export default class Main extends BaseController {
+  formatIncidence = (incidence: number) => {
+    return Math.round(incidence);
+  };
 
-	public sayHello() : void {
-		MessageBox.show("Hello World!");
-	}
-
+  formatIconColor = (incidence: number) => {
+    if (incidence < 25) return IconColor.Default;
+    if (incidence < 30) return IconColor.Critical;
+    return IconColor.Negative;
+  };
 }
