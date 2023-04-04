a6a7882ca2ee82cef916840152387eb8231e5aba
gnos28 <gnos28@gmail.com>
2023-04-04 11:37:06 +0200

    exo5 done

---
+++ src/controller/IncidenceDetail.controller.ts
@@ -0,0 +1,8 @@
+import BaseController from "./BaseController";
+
+/**
+ * @name com.myorg.tsexo1.controller.IncidenceDetail
+ */
+export default class IncidenceDetail extends BaseController {
+    
+}
--- src/controller/Main.controller.ts
+++ src/controller/Main.controller.ts
@@ -1,5 +1,37 @@
 import { IconColor } from "sap/ui/core/library";
 import BaseController from "./BaseController";
+import Event from "sap/ui/base/Event";
+import Control from "sap/ui/core/Control";
+
+type Incidence = {
+  id: number;
+  name: string;
+  population: number;
+  cases: number;
+  deaths: number;
+  casesPerWeek: number;
+  deathsPerWeek: number;
+  recovered: number;
+  abbreviation: string;
+  weekIncidence: number;
+  casesPer100k: number;
+  delta: Delta;
+  hospitalization: Hospitalization;
+};
+
+type Delta = {
+  cases: number;
+  deaths: number;
+  recovered: number;
+  weekIncidence: number;
+};
+
+type Hospitalization = {
+  cases7Days: number;
+  incidence7Days: number;
+  date: string;
+  lastUpdate: string;
+};
 
 /**
  * @namespace com.myorg.tsexo1.controller
@@ -14,4 +46,16 @@ export default class Main extends BaseController {
     if (incidence < 30) return IconColor.Critical;
     return IconColor.Negative;
   };
+
+  navToIncidenceDetail(event: Event) {
+    const stateObj = (event.getSource() as Control)
+      .getBindingContext()
+      .getObject() as Incidence;
+
+    const stateId = stateObj.abbreviation;
+
+    console.log(stateId);
+    
+    this.navTo("IncidenceDetailRoute", { id: stateId });
+  }
 }
